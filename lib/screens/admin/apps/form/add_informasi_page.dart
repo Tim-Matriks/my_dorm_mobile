26c97ae85edfcdd88e7fa587dacb581d667a63e7
rakha <rakhagalih19@gmail.com>
2024-10-10 21:31:04 +0700

    add informasi

---
+++ lib/screens/admin/apps/form/add_informasi_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddInformasiPage extends StatelessWidget {
+  const AddInformasiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
