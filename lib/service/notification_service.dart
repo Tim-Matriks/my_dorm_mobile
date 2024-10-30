53d748a199273b9578b3a600d6f4e9895dd7d787
IksanRisandy <iksanrisandy@gmail.com>
2024-10-30 13:21:41 +0700

    menambahkan notification service

---
+++ lib/service/notification_service.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+import 'dart:developer';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
+
+class MyNotificationService {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  Future<void> initialization() async {
+    tz.initializeTimeZones();
+    _requestPermissions();
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = const DarwinInitializationSettings();
+    var initSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    flutterLocalNotificationsPlugin.initialize(
+      initSettings,
+      onDidReceiveNotificationResponse: onSelectNotification,
+    );
+  }
+
+  Future<void> _requestPermissions() async {
+    if (await Permission.notification.isDenied) {
+      await Permission.notification.request();
+    }
+  }
+
+  void onSelectNotification(NotificationResponse notificationResponse) async {
+    if (notificationResponse.payload != null) {
+      //navigasi ke halaman notif
+    }
+  }
+
+  Future<void> showNotification() async {
+    var androidDetails = const AndroidNotificationDetails(
+      'id',
+      'channel',
+      channelDescription: 'description',
+      priority: Priority.high,
+      importance: Importance.max,
+    );
+    var iOSDetails = const DarwinNotificationDetails();
+    var platformDetails = NotificationDetails(
+      android: androidDetails,
+      iOS: iOSDetails,
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Peringatan Jam Malam',
+      'Halo dormitizen, sebentar lagi akan memasuki jam malam, harap segera kembali ke asrama ya',
+      platformDetails,
+      payload: 'MyDorm Peringatan Jam Malam',
+    );
+    log("berhasil");
+  }
+
+  Future<void> scheduleDailyNotification(DateTime selectedTime) async {
+    if (selectedTime.isBefore(DateTime.now())) {
+      selectedTime = selectedTime.add(const Duration(days: 1));
+      log("akan diberikan notifikasi besok pada jam ${selectedTime.toString()}");
+    }
+    final tz.TZDateTime scheduledTime =
+        tz.TZDateTime.from(selectedTime, tz.local);
+
+    try {
+      await flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        "Peringatan Jam Malam!",
+        "Halo dormitizen, sebentar lagi asrama akan dikunci oleh karena itu mohon segera kembali ke asrama",
+        scheduledTime,
+        _notificationDetails(),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+      );
+
+      log('Notification scheduled successfully');
+    } catch (e) {
+      log('Error scheduling notification: $e');
+    }
+  }
+
+  NotificationDetails _notificationDetails() {
+    return const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'your_channel_id',
+        'your_channel_name',
+        importance: Importance.max,
+        priority: Priority.high,
+        showWhen: false,
+      ),
+      iOS: DarwinNotificationDetails(),
+    );
+  }
+}
