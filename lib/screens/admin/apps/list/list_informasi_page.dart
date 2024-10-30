22aeb914b01ff0c52a13f0c6a47211987515aa5d
Dias <78405123+diasdigital@users.noreply.github.com>
2024-10-30 09:04:56 +0700

    Menambah dialog hapus di halaman list informasi ketika menghapus informasi

--- lib/screens/admin/apps/list/list_informasi_page.dart
+++ lib/screens/admin/apps/list/list_informasi_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_dorm/components/appbar_page.dart';
 import 'package:my_dorm/constant/constant.dart';
+import 'package:my_dorm/screens/admin/apps/form/add_informasi_page.dart';
 
 class ListInformasiPage extends StatelessWidget {
   const ListInformasiPage({super.key});
@@ -11,7 +12,12 @@ class ListInformasiPage extends StatelessWidget {
         body: Column(children: [
       AppBarPage(
         title: 'Informasi',
-        onAdd: () {},
+        onAdd: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const AddInformasiPage()));
+        },
       ),
       Padding(
         padding: const EdgeInsets.all(16.0),
@@ -53,14 +59,80 @@ class ListInformasiPage extends StatelessWidget {
           ],
         ),
       ),
-      Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Container(
-          height: 200,
-          decoration: BoxDecoration(
-              gradient: kGradientBlue, borderRadius: BorderRadius.circular(10)),
+      Container(
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.all(16),
+        height: 200,
+        decoration: BoxDecoration(
+            gradient: kGradientBlue, borderRadius: BorderRadius.circular(10)),
+        child: Stack(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Pengurasan Air',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      '11 November 2023',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white70,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                GestureDetector(
+                  child: Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Hapus informasi'),
+                          content: Text(
+                              'Apakah anda yakin ingin menghapus informasi ini?'),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('Tidak'),
+                              onPressed: () {
+                                Navigator.of(context)
+                                    .pop(); // kalau tidak jadi hapus
+                              },
+                            ),
+                            TextButton(
+                              child: const Text('Ya'),
+                              onPressed: () {
+                                Navigator.of(context).pop(); // kalau jadi hapus
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            )
+          ],
         ),
-        // TODO: Melengkapi isi card
       )
     ]));
   }
