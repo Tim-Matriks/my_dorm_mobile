08892a609832c4794b1acac974edc5356a24ae98
IksanRisandy <iksanrisandy@gmail.com>
2024-12-14 19:41:48 +0700

    menambahkan firebase messaging service

--- lib/service/push_notification_service.dart
+++ lib/service/push_notification_service.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:uuid/uuid.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -7,23 +9,12 @@ import 'package:googleapis/servicecontrol/v1.dart' as serviceControl;
 
 class PushNotificationService {
   static Future<String> getAccessToken() async {
-    final serviceAccountJson = {
-      "type": "service_account",
-      "project_id": "my-dorm-f2cad",
-      "private_key_id": "ee5e17b453cf4937a4cfaaeccb2340204bc09962",
-      "private_key":
-          "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC0u9XXiG7UkarI\n27l/dwl1fYutDSE+KYTeK6qtLfVE5tXmY5a+EJ3DDzRp/o3rSdllnizRA6R+oNuh\nVC3uzz0pVhFWDzKsVOcoDc6WcsvMLLKUyqYkWy79RjPCMxDnN68gsVDDo/rKIRYh\ndj2LNt5q+In/7NCGNmYXCzTfCIlLWcuuMGljXtGvS7HIwMZv/+npJguf8AvQ7QMl\nOoDt4ziYM75CHNdMFdRx0+U3T9iVEtQ36yl8O3cxYVt4JKa7iSWb6woK8LKZdjb0\n/LSaVGbvP7uKnkL6qMW1aKs74LK7y0jxat/1zqT+o3wdoPo0Ky0CAjCmlHQ9TT10\nQrzJA4QHAgMBAAECggEABtEu97pFs+7qqjjffldXG/9oTVa5ckbOpDKxdWbOYZLz\nboVDIlguGzVvnK/lWoKmmJamyhuBEOWwYx1xvhhDDlQYw04/cxCgx3n92N7roP/u\nnH4oVia6VwNW4HRcptwK1wjZKwf8ZbNpyklTePTQ3ugRDH46P1W99BCQJfmEdDsW\noNrV4nqEv8lW67zO3RKAmuEvnpPdVcBXrSUbtOloNtyWmBMc/YgultQMPiMbghu1\nQAT4J0FeP5GaBC++dVGDyJhyiwhw4PQIk3ynyW6EVKvKUKy4AocILmOzrcMPnSfI\nd0dXB1QlnK9vbNDLK6x86FvuCsj4pek5YxQpsOUfwQKBgQDbLyHtFFQEnl4l4wwo\nlD5Tupz3cuq8rwKByTlDK1qDIW3yEPadw9Bit7WESZcN4VGAXtGPj+Z6knPnwnFa\nxICglLb8LirSkuInSg9E3nnnrbm7h8cyZoOKxAZmc+iId2MrhgKbH1Xgqg/mCoYL\nAXibOdMK7cYeUNMgnGrXFXSQIwKBgQDTF1hZynGcxpAP7ZqqF0Pl/KQo6Y2aW1JU\n4bkTTFxEw1LnDIT5rDBONHXhHddGbxKVvYfFZSHhLxkyfS53WaAEZTY27QiTqUeu\nyQoYHPYRkQt9ej25qp0URXyQ2X96m3Wd7lO7crm0gMqrLMPyDJ90SIL9Qm+Ik6LH\n5IY2XlkIzQKBgD7y7GVwfyjMavGqoaVN3hCh+c6/fFcwVCH+Lqnx247pnO/2mz14\nLXMMm98gW3erDIM3uCvpAiKVyR/4oiGVafO0glEu2TGc6cKq7HO1CYHWTQ9k0XrS\nIllKLyRhiZkX4K1xUXJHr5xUezhwMUsb4w2Br/DllQ7D277z7WNUUCPNAoGALxpm\nfKLP90vXmbZLbOkEHa3ic76p+memV4qG4eeIL0/mj+gbRBtILCtIopMmFnFylDdW\ntJV8meMInFuSos89Bb4P+vGpmEmN1VLHDdpojkLvbXB5lZScuIlI4ommCJXoCPXF\neW6AQeb7UncKwlhOvIPxttG8UZ2gFTrJOBiVhYECgYA9/YVlSZ2NE9c4x1c6Lu9r\nMaMYMlIzwPUFvO2MuPeUXJJrd3DOosq7wfx/BaO3kJvF3JN7hG/yjREXodG27WLn\nT9PxrnRfSsgzZdQv/A/qk5eqs5UZgJ/itflIj41fWSw8zevvl2bDNjb6P0IVE9yN\nq7cN9b15aN88dtSYxlDr7A==\n-----END PRIVATE KEY-----\n",
-      "client_email":
-          "firebase-adminsdk-upmnl@my-dorm-f2cad.iam.gserviceaccount.com",
-      "client_id": "106328549556625958347",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url":
-          "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url":
-          "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-upmnl%40my-dorm-f2cad.iam.gserviceaccount.com",
-      "universe_domain": "googleapis.com"
-    };
+    // Menggunakan rootBundle untuk mengakses file yang ada di assets
+    String contents =
+        await rootBundle.loadString('assets/service_account.json');
+
+    // Mengubah isi JSON menjadi Map
+    Map<String, dynamic> serviceAccountJson = jsonDecode(contents);
 
     List<String> scopes = [
       'https://www.googleapis.com/auth/firebase.messaging'
