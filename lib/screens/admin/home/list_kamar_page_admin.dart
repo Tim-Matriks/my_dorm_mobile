9b892c6ef99fb3595bd5ad4d3a1c3b72c9311bb3
zidan <zidansivafajar23@gmail.com>
2025-01-06 08:50:09 +0700

    Integrasi API untuk tampilan kamar di halaman SR

--- lib/screens/admin/home/list_kamar_page_admin.dart
+++ lib/screens/admin/home/list_kamar_page_admin.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_dorm/components/appbar_page.dart';
 import 'package:my_dorm/constant/constant.dart';
+import 'package:my_dorm/service/http_service.dart';
 
 class ListKamarPageAdmin extends StatefulWidget {
   const ListKamarPageAdmin({super.key});
@@ -10,35 +11,56 @@ class ListKamarPageAdmin extends StatefulWidget {
 }
 
 class _ListKamarPageAdminState extends State<ListKamarPageAdmin> {
-  int _selectedIndex = 0;
-  List<Map> listKamar = [
-    {'nomor': 101, 'ada': true},
-    {'nomor': 102, 'ada': true},
-    {'nomor': 103, 'ada': false},
-    {'nomor': 104, 'ada': true},
-    {'nomor': 105, 'ada': true},
-    {'nomor': 106, 'ada': true},
-    {'nomor': 107, 'ada': true},
-    {'nomor': 108, 'ada': false},
-    {'nomor': 109, 'ada': true},
-    {'nomor': 110, 'ada': true},
-    {'nomor': 111, 'ada': false},
-    {'nomor': 112, 'ada': true},
-    {'nomor': 113, 'ada': true},
-    {'nomor': 114, 'ada': false},
-    {'nomor': 115, 'ada': true},
-    {'nomor': 116, 'ada': true},
-    {'nomor': 117, 'ada': false},
-    {'nomor': 118, 'ada': true},
-    {'nomor': 119, 'ada': true},
-    {'nomor': 120, 'ada': true},
-    {'nomor': 121, 'ada': true},
-    {'nomor': 122, 'ada': false},
-    {'nomor': 123, 'ada': true},
-    {'nomor': 124, 'ada': true},
-  ];
+  List<Map<String, dynamic>> kamars = [];
+  String error = "";
+  bool _showSpinner = false;
 
-  //TODO: jadikan datanya lengkap tiap lantai, namun mungkin taro di file lain agar tidak penuh disini
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getKamar();
+  }
+
+  Future<void> getKamar() async {
+    setState(() {
+      _showSpinner = true;
+      error = "";
+    });
+    try {
+      String? token = await getToken();
+      var response = await getDataToken('/kamar/status/all', token!);
+      if (response['data'] != null) {
+        setState(() {
+          kamars = (response['data'] as List)
+              .map((item) => item as Map<String, dynamic>)
+              .toList();
+        });
+        print('Data Kamar: $kamars');
+      } else {
+        setState(() {
+          error = "Data kamar dormitizen kosong.";
+        });
+      }
+    } catch (e) {
+      print(e);
+      setState(() {
+        error = "Error: $e";
+      });
+    } finally {
+      setState(() {
+        _showSpinner = false;
+      });
+    }
+  }
+
+  /// Filter kamar berdasarkan lantai
+  List<Map<String, dynamic>> getKamarByLantai(int lantai) {
+    return kamars
+        .where((kamar) => kamar['nomor'].startsWith('$lantai'))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +71,19 @@ class _ListKamarPageAdminState extends State<ListKamarPageAdmin> {
             title: 'Kamar',
             canBack: false,
           ),
-          SizedBox(height: 10),
-          Container(
-            height: 630,
-            child: PageView.builder(
+          const SizedBox(height: 10),
+          if (_showSpinner)
+            const Center(child: CircularProgressIndicator())
+          else if (error.isNotEmpty)
+            Center(
+              child: Text(
+                error,
+                style: TextStyle(color: Colors.red),
+              ),
+            )
+          else
+            Expanded(
+              child: PageView.builder(
                 onPageChanged: (value) {
                   setState(() {
                     _selectedIndex = value;
@@ -61,57 +92,99 @@ class _ListKamarPageAdminState extends State<ListKamarPageAdmin> {
                 itemCount: 4,
                 itemBuilder: (context, pageIndex) {
                   int floorNumber = pageIndex + 1;
-                  return Column(children: [
-                    Text(
-                      'Lantai $floorNumber',
-                      style: kBoldTextStyle.copyWith(fontSize: 24),
-                    ),
-                    SizedBox(height: 10),
-                    Expanded(
-                      child: GridView.count(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          crossAxisCount: 4,
-                          crossAxisSpacing: 12,
-                          mainAxisSpacing: 12,
-                          children: List.generate(24, (index) {
-                            return Center(
-                              child: Container(
+                  List<Map<String, dynamic>> kamarLantai =
+                      getKamarByLantai(floorNumber);
+
+                  int kamarCount = (floorNumber == 1) ? 20 : 24;
+
+                  return Column(
+                    children: [
+                      Text(
+                        'Lantai $floorNumber',
+                        style: kBoldTextStyle.copyWith(fontSize: 24),
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: GridView.builder(
+                          padding:
+                              const EdgeInsets.symmetric(horizontal: 20),
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 12,
+                            mainAxisSpacing: 12,
+                          ),
+                          itemCount: kamarCount,
+                          itemBuilder: (context, index) {
+                            if (index < kamarLantai.length) {
+                              var kamar = kamarLantai[index];
+                              return Container(
                                 decoration: BoxDecoration(
-                                    color: (listKamar[index]['ada'])
-                                        ? kRed
-                                        : kGray,
-                                    borderRadius: BorderRadius.circular(5)),
+                                  color: (kamar['status'] == 'terkunci')
+                                      ? kGray
+                                      : kRed,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
                                 child: Center(
-                                    child: Text(
-                                  '${listKamar[index]['nomor']}',
-                                  textAlign: TextAlign.center,
-                                  style: kBoldTextStyle.copyWith(
-                                      fontSize: 24, color: kWhite),
-                                )),
-                              ),
-                            );
-                          })),
-                    ),
-                  ]);
-                }),
-          ),
+                                  child: Text(
+                                    '${kamar['nomor']}',
+                                    textAlign: TextAlign.center,
+                                    style: kBoldTextStyle.copyWith(
+                                      fontSize: 24,
+                                      color: (kamar['status'] == 'terkunci')
+                                      ? Colors.black.withOpacity(0.4)
+                                      : kWhite,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            } else {
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: kGray,
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    'N/A',
+                                    textAlign: TextAlign.center,
+                                    style: kBoldTextStyle.copyWith(
+                                      fontSize: 24,
+                                      color: kWhite,
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          const SizedBox(height: 10),
           Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: List.generate(
-                4,
-                (index) => AnimatedContainer(
-                  margin: const EdgeInsets.only(right: 6),
-                  height: 5,
-                  width: (index == _selectedIndex) ? 25 : 12,
-                  decoration: BoxDecoration(
-                      color: (index == _selectedIndex)
-                          ? Colors.black.withOpacity(0.75)
-                          : Colors.black.withOpacity(0.25),
-                      borderRadius: BorderRadius.circular(2.5)),
-                  duration: const Duration(milliseconds: 450),
-                  curve: Curves.easeInOutQuint,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(
+              4,
+              (index) => AnimatedContainer(
+                margin: const EdgeInsets.only(right: 6),
+                height: 5,
+                width: (index == _selectedIndex) ? 25 : 12,
+                decoration: BoxDecoration(
+                  color: (index == _selectedIndex)
+                      ? Colors.black.withOpacity(0.75)
+                      : Colors.black.withOpacity(0.25),
+                  borderRadius: BorderRadius.circular(2.5),
                 ),
-              ))
+                duration: const Duration(milliseconds: 450),
+                curve: Curves.easeInOutQuint,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
         ],
       ),
     );
