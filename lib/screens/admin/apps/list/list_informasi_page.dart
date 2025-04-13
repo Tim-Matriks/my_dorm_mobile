b528e6406c0db5530586e557deda7e8126a2f37a
rakha <rakhagalih19@gmail.com>
2025-04-13 22:35:21 +0700

    ANJAY BISA (NGATASIN ERROR FLUTTER UPGRADE)

--- lib/screens/admin/apps/list/list_informasi_page.dart
+++ lib/screens/admin/apps/list/list_informasi_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:my_dorm/components/appbar_page.dart';
 import 'package:my_dorm/constant/constant.dart';
 import 'package:my_dorm/screens/admin/apps/form/add_informasi_page.dart';
+import 'package:my_dorm/service/http_service.dart';
+import 'package:my_dorm/service/image_service.dart';
 
 class ListInformasiPage extends StatefulWidget {
   const ListInformasiPage({super.key});
@@ -11,155 +14,171 @@ class ListInformasiPage extends StatefulWidget {
 }
 
 class _ListInformasiPageState extends State<ListInformasiPage> {
-  final List<Map<String, dynamic>> informasi = [
-    {
-      "author": "Rakha Galih Nugraha S",
-      "date": "12 Januari 2025",
-      "title": "Open Recruitment Senior Residents XIII",
-      "excerpt":
-          "Formulir ini adalah langkah pertama bagi Dormitizen dan TeluTizen yang ingin menjadi bagian dari Senior Resident XIII...",
-      "imageUrl":
-          "https://via.placeholder.com/300x150", // ganti dengan URL gambar asli
-    },
-    {
-      "author": "Iksan Oktav risandy",
-      "date": "19 Desember 2024",
-      "title": "Open Recruitment Senior Residents XII",
-      "excerpt":
-          "Formulir ini adalah langkah pertama bagi Dormitizen dan TeluTizen yang ingin menjadi bagian dari Senior Resident XII...",
-      "imageUrl":
-          "https://via.placeholder.com/300x150", // ganti dengan URL gambar asli
-    },
-  ];
+  List<Map<String, dynamic>> beritas = [];
+  String error = "";
+  bool _showSpinner = false;
+  @override
+  void initState() {
+    super.initState();
+    getBerita();
+  }
+
+  Future<void> getBerita() async {
+    error = "";
+    setState(() {
+      _showSpinner = true;
+    });
+    try {
+      String? token = await getToken();
+      var response = await getDataToken('/berita', token!);
+      List<Map<String, dynamic>> parsedData = (response['data'] as List)
+          .map((item) => item as Map<String, dynamic>)
+          .toList();
+      print(response);
+      setState(() {
+        beritas = parsedData;
+      });
+    } catch (e) {
+      print(e);
+      setState(() {
+        error = "Error: $e";
+      });
+    } finally {
+      setState(() {
+        _showSpinner = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          AppBarPage(
-            title: 'Informasi',
-            onAdd: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AddInformasiPage(),
-                ),
-              );
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 50,
-                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: kGrey),
-                    ),
-                    child: Row(
-                      children: const [
-                        Icon(Icons.search),
-                        SizedBox(width: 5),
-                        Text('Cari')
-                      ],
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    border: Border.all(color: kGrey),
-                  ),
-                  child: const Icon(Icons.filter_alt),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              itemCount: informasi.length,
-              itemBuilder: (context, index) {
-                final item = informasi[index];
-                return Card(
-                  margin: const EdgeInsets.symmetric(vertical: 10),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12)),
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            const CircleAvatar(
-                              backgroundImage: AssetImage(
-                                  'assets/avatar.png'), // ganti path jika perlu
-                              radius: 20,
-                            ),
-                            const SizedBox(width: 10),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(item['author'],
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold)),
-                                Text(item['date'],
-                                    style: const TextStyle(
-                                        color: Colors.grey, fontSize: 12)),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        Text(
-                          item['title'],
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        const SizedBox(height: 6),
-                        Text.rich(
-                          TextSpan(
-                            text: item['excerpt'],
-                            children: [
-                              TextSpan(
-                                text: ' Baca Selengkapnya',
-                                style: const TextStyle(
-                                    color: Colors.red,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: Image.network(
-                            item['imageUrl'],
-                            height: 140,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ],
-                    ),
+      body: ModalProgressHUD(
+        inAsyncCall: _showSpinner,
+        child: Column(
+          children: [
+            AppBarPage(
+              title: 'Informasi',
+              onAdd: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AddInformasiPage(),
                   ),
                 );
               },
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(color: kGrey),
+                      ),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.search),
+                          SizedBox(width: 5),
+                          Text('Cari')
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: kGrey),
+                    ),
+                    child: const Icon(Icons.filter_alt),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                itemCount: beritas.length,
+                itemBuilder: (context, index) {
+                  final item = beritas[index];
+                  return Card(
+                    color: kWhite,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12)),
+                    elevation: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              const CircleAvatar(
+                                backgroundImage: AssetImage(
+                                    'images/zee.png'), // ganti path jika perlu
+                                radius: 20,
+                              ),
+                              const SizedBox(width: 10),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text('ayay',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold)),
+                                  Text(item['updated_at'] ?? 'error',
+                                      style: const TextStyle(
+                                          color: Colors.grey, fontSize: 12)),
+                                ],
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            item['judul'] ?? 'error',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
+                          Text(
+                            item['isi'] ?? 'error',
+                            maxLines: 5,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const Text(
+                            'Baca Selengkapnya',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 10),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: MyNetworkImage(
+                              imageURL:
+                                  'https://mydorm-mobile-backend-production.up.railway.app/images/${item['gambar']}',
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
