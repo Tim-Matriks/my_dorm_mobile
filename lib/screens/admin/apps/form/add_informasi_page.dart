b9dbaee6353a587c670ecc77602debac38792d0a
IksanRisandy <iksanrisandy@gmail.com>
2025-05-05 15:45:20 +0700

    menambahkan fungsi cari dormitizen di form

--- lib/screens/admin/apps/form/add_informasi_page.dart
+++ lib/screens/admin/apps/form/add_informasi_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:my_dorm/components/appbar_page.dart';
 import 'package:my_dorm/components/form_drop_down.dart';
@@ -5,6 +7,7 @@ import 'package:my_dorm/components/form_photo_picker.dart';
 import 'package:my_dorm/components/form_textfield.dart';
 import 'package:my_dorm/components/gradient_button.dart';
 import 'package:my_dorm/constant/constant.dart';
+import 'package:my_dorm/service/http_service.dart';
 import 'package:my_dorm/service/online_notification_service.dart';
 
 class AddInformasiPage extends StatefulWidget {
@@ -17,8 +20,44 @@ class AddInformasiPage extends StatefulWidget {
 class _AddInformasiPageState extends State<AddInformasiPage> {
   final TextEditingController _judulController = TextEditingController();
   final TextEditingController _deskripsiController = TextEditingController();
-
+  String? kategori;
   final _formKey = GlobalKey<FormState>();
+  String error = "";
+  String waktu = "";
+  File? gambar;
+  bool _showSpinner = false;
+
+  Future<void> _addInformasi() async {
+    error = "";
+    setState(() {
+      _showSpinner = true;
+    });
+    dynamic response = {};
+    try {
+      Map<String, String> data = {
+        'judul': _judulController.text,
+        'isi': _deskripsiController.text,
+        'kategori': kategori!,
+      };
+      response = await postDataTokenWithImage("/berita", data, gambar);
+      print('berhasil tambah laporan!');
+      if (mounted) {
+        Navigator.pop(context, 'sesuatu');
+      }
+
+      print(response['message']);
+    } catch (e) {
+      setState(() {
+        _showSpinner = false;
+        error = "${response['message']}";
+      });
+      print('Login error: $e');
+      print(response);
+    }
+    setState(() {
+      _showSpinner = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,13 +80,16 @@ class _AddInformasiPageState extends State<AddInformasiPage> {
                     FormPhotoPicker(
                       title: 'informasi',
                       onImageSelected: (selectedImage) {
-            // Handle the selected image here
-            if (selectedImage != null) {
-              print('Selected image path: ${selectedImage.path}');
-            } else {
-              print('Image cleared');
-            }
-          },
+                        // Handle the selected image here
+                        if (selectedImage != null) {
+                          print('Selected image path: ${selectedImage.path}');
+                          setState(() {
+                            gambar = selectedImage;
+                          });
+                        } else {
+                          print('Image cleared');
+                        }
+                      },
                     ),
                     FormDropDown(
                         title: 'Kategori',
@@ -59,6 +101,9 @@ class _AddInformasiPageState extends State<AddInformasiPage> {
                         ],
                         onItemSelected: (selectedItem) {
                           // Handle the selected item here
+                          setState(() {
+                            kategori = selectedItem!;
+                          });
                           print('Selected item: $selectedItem');
                         }),
                     FormTextField(label: 'Judul', controller: _judulController),
@@ -70,6 +115,7 @@ class _AddInformasiPageState extends State<AddInformasiPage> {
                     GradientButton(
                         ontap: () {
                           if (_formKey.currentState?.validate() ?? false) {
+                            _addInformasi();
                             MyNotificationService.sendNotificationToAllUsers(
                                 context,
                                 _judulController.text,
--- lib/screens/admin/apps/form/add_paket_page.dart
+++ lib/screens/admin/apps/form/add_paket_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 import 'package:my_dorm/components/appbar_page.dart';
 import 'package:my_dorm/components/form_date_time_picker.dart';
 import 'package:my_dorm/components/form_dormitizen_picker.dart';
@@ -6,6 +10,7 @@ import 'package:my_dorm/components/form_photo_picker.dart';
 import 'package:my_dorm/components/form_textfield.dart';
 import 'package:my_dorm/components/gradient_button.dart';
 import 'package:my_dorm/constant/constant.dart';
+import 'package:my_dorm/service/http_service.dart';
 
 class AddPaketPage extends StatefulWidget {
   const AddPaketPage({super.key});
@@ -17,6 +22,84 @@ class AddPaketPage extends StatefulWidget {
 class _AddPaketPageState extends State<AddPaketPage> {
   final TextEditingController _namaBarangController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  String? selectedDormitizen;
+  final List<Map<String, dynamic>> dormitizenDataList = [];
+  final TextEditingController _kamarController = TextEditingController();
+  String error = "";
+  String waktu = "";
+  File? gambar;
+  bool _showSpinner = false;
+
+  Future<void> _addPaket() async {
+    error = "";
+    setState(() {
+      _showSpinner = true;
+    });
+    dynamic response = {};
+    try {
+      Map<String, String> data = {
+        'status_pengambilan': 'belum',
+        'waktu_tiba': waktu,
+        'dormitizen_id': selectedDormitizen!,
+      };
+      response = await postDataTokenWithImage("/paket", data, gambar);
+      print('berhasil tambah laporan!');
+      if (mounted) {
+        Navigator.pop(context, 'sesuatu');
+      }
+
+      print(response['message']);
+    } catch (e) {
+      setState(() {
+        _showSpinner = false;
+        error = "${response['message']}";
+      });
+      print('Login error: $e');
+      print(response);
+    }
+    setState(() {
+      _showSpinner = false;
+    });
+  }
+
+  Future<void> searchDormitizen(String nomorKamar) async {
+    error = "";
+    setState(() {
+      _showSpinner = true;
+    });
+    try {
+      dormitizenDataList.clear();
+      String? token = await getToken();
+      var response = await getDataToken('/user/$nomorKamar', token!);
+
+      if (response['response'] != null) {
+        List<Map<String, dynamic>> dormitizens = (response['response'] as List)
+            .map((item) => item as Map<String, dynamic>)
+            .toList();
+        for (var dormitizen in dormitizens) {
+          dormitizenDataList.add({
+            'id': dormitizen['dormitizen_id'],
+            'nama': dormitizen['nama'],
+          });
+        }
+        print('Data Dormitizen: $dormitizens');
+      } else {
+        setState(() {
+          error = "Data dormitizen tidak ditemukan.";
+        });
+      }
+    } catch (e) {
+      print(e);
+      setState(() {
+        error = "Error: $e";
+      });
+    } finally {
+      setState(() {
+        _showSpinner = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,27 +118,93 @@ class _AddPaketPageState extends State<AddPaketPage> {
                     const SizedBox(
                       height: 12,
                     ),
-                    const FormDormitizenPicker(),
-                    FormPhotoPicker(title: 'paket',onImageSelected: (selectedImage) {
-            // Handle the selected image here
-            if (selectedImage != null) {
-              print('Selected image path: ${selectedImage.path}');
-            } else {
-              print('Image cleared');
-            }
-          },),
-                    FormTextField(
-                        label: 'Nama barang',
-                        controller: _namaBarangController),
+                    // const FormDormitizenPicker(),
+                    TextFormField(
+                      controller: _kamarController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      decoration: InputDecoration(
+                        labelText: 'Nomor Kamar',
+                        hintText: 'Masukkan nomor kamar',
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: kMain, width: 2),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () async {
+                            await searchDormitizen(_kamarController.text);
+                          },
+                          icon: const Icon(Icons.search),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Nomor kamar tidak boleh kosong';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 12),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: DropdownButtonFormField<String>(
+                        style: kMediumTextStyle.copyWith(
+                            fontSize: 16, color: Colors.black),
+                        decoration: basicInputDecoration("Pilih Dormitizen"),
+                        value: selectedDormitizen,
+                        icon: const Icon(Icons.arrow_drop_down),
+                        isExpanded: true,
+                        items: dormitizenDataList
+                            .map((Map<String, dynamic> dormitizen) {
+                          return DropdownMenuItem<String>(
+                            value: dormitizen['id'].toString(),
+                            child: Text(dormitizen['nama']),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            selectedDormitizen = newValue;
+                          });
+                          print('Selected Dormitizen: $selectedDormitizen');
+                        },
+                      ),
+                    ),
+                    FormPhotoPicker(
+                      title: 'paket',
+                      onImageSelected: (selectedImage) {
+                        // Handle the selected image here
+                        if (selectedImage != null) {
+                          print('Selected image path: ${selectedImage.path}');
+                          setState(() {
+                            gambar = selectedImage;
+                          });
+                        } else {
+                          print('Image cleared');
+                        }
+                      },
+                    ),
+                    // FormTextField(
+                    //     label: 'Nama barang',
+                    //     controller: _namaBarangController),
                     FormDatePicker(
                       onDateTimeSelected: (selectedDateTime) {
-            // Handle the combined DateTime here
-            print('Selected DateTime: $selectedDateTime');
-          },
+                        // Handle the combined DateTime here
+                        print('Selected DateTime: $selectedDateTime');
+                        String formattedDate = DateFormat('yyyy-MM-dd HH:mm:ss')
+                            .format(selectedDateTime!);
+                        waktu = formattedDate;
+                      },
                     ),
                     GradientButton(
                         ontap: () {
                           if (_formKey.currentState?.validate() ?? false) {
+                            _addPaket();
                             final snackBar = SnackBar(
                               content: const Text('Data berhasil ditambahkan!'),
                               action: SnackBarAction(
